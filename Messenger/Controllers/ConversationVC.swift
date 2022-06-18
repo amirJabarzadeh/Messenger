5533129d83ac65a0dd416929ff882d50699d15af
karenbizllc <karenbizllc@gmail.com>
2022-06-18 19:13:20 +0430

    first commit

---
+++ Messenger/Controllers/ConversationVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by developer on 6/18/22.
+//
+
+import UIKit
+
+class ConversationVC : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
---
+++ Messenger/Controllers/RegisterVC.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterVC.swift
+//  Messenger
+//
+//  Created by developer on 6/18/22.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
